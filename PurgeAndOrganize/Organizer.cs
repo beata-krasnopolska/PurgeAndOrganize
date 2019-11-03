f46e7e74e644c010241599001e1d0d2f2f3851a7
beata.krasnopolska@gmail.com <beata.krasnopolska@gmail.com>
2019-11-03 20:36:00 +0100

    Add project files.

---
+++ PurgeAndOrganize/Organizer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace PurgeAndOrganize
+{
+    class Organizer
+    {
+        public void Organize(int[] array)
+        {
+            var newArray = array.Distinct().OrderBy(p => p).ToArray();
+
+            foreach (var p in newArray)
+            {
+                Console.Write(p);
+            }
+        }
+    }
+}
---
+++ PurgeAndOrganize/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace PurgeAndOrganize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program returns an array that has all duplicate elements removed, is sorted from least value to greatest value.");
+
+            int[] array_1 = new int[] {1, 2, 4, 3};
+            int[] array_2 = new int[] { 1, 4, 4, 4, 4, 4, 3, 2, 1, 2 };
+            int[] array_3 = new int[] { 6, 7, 3, 2, 1 };
+
+            var sorted = new Organizer();
+            Console.WriteLine("Array 1:");
+            sorted.Organize(array_1);
+            Console.WriteLine();
+            Console.WriteLine("Array 2:");
+            sorted.Organize(array_2);
+            Console.WriteLine();
+            Console.WriteLine("Array 3:");
+            sorted.Organize(array_3);
+
+            Console.ReadKey();
+        }
+    }
+}
